4846d69af4c2e3dfcdff34555a5acfe0b5a58978
Piotr Dopierala <ptr.dopierala@gmail.com>
2019-05-04 20:52:21 +0200

    - added response body in error response handlers.

--- src/main/java/pl/dopierala/allegroreporeaderapi/aop/ApplicationErrorHandler.java
+++ src/main/java/pl/dopierala/allegroreporeaderapi/aop/ApplicationErrorHandler.java
@@ -1,5 +1,6 @@
 package pl.dopierala.allegroreporeaderapi.aop;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -10,11 +11,11 @@ import pl.dopierala.allegroreporeaderapi.exceptions.UserNotFoundException;
 public class ApplicationErrorHandler {
     @ExceptionHandler(UserNotFoundException.class)
     public ResponseEntity handleUserNotFoundException() {
-        return ResponseEntity.notFound().build();
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body("User not found");
     }
 
     @ExceptionHandler(ParseToJsonNotPossibleException.class)
     public ResponseEntity handleParseToJsonNotPossibleException(){
-        return ResponseEntity.notFound().build();
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Response from GitHub invalid. Can't parse Json to object.");
     }
 }
