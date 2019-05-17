a9c098ffa4dd116ead80f01a975c024996a9865d
Piotr Dopierala <ptr.dopierala@gmail.com>
2019-05-17 13:22:40 +0200

    changed security configuration.

--- src/main/java/pl/dopierala/allegroreporeaderapi/configuration/SecurityConfig.java
+++ src/main/java/pl/dopierala/allegroreporeaderapi/configuration/SecurityConfig.java
@@ -1,6 +1,7 @@
 package pl.dopierala.allegroreporeaderapi.configuration;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -14,8 +15,11 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
-                .antMatchers("/**")
+                .antMatchers(HttpMethod.OPTIONS,"/**")
                 .permitAll()
+                .anyRequest().fullyAuthenticated()
+                .and()
+                .httpBasic()
                 .and()
                 .csrf()
                 .disable()
